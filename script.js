49729995e1493f1ebb568d884dd4933518040b4d
YoungHa Kim <ytok1108@aol.com>
2021-11-01 01:57:38 +0900

    ^^; 4th gogo

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 const date = new Date();
 
+const monthDays = document.querySelector(".days");
+
+const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+
 const months = [
     "January",
     "February",
     "March",
     "April",
+    "May",
     "June",
     "July",
     "August",
@@ -18,3 +23,9 @@ document.querySelector(".date h1").innerHTML=months[date.getMonth()];
 
 document.querySelector(".date p").innerHTML= date.toDateString();
 
+let days = "";
+
+for (let i = 1; i<= lastDay; i++) {
+    days += `<div>${i}</div>`;
+    monthDays.innerHTML = days;
+}
